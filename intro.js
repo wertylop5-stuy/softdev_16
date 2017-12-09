2d0c2c1da9386331330ea89ae0de9538f88e5377
Stanley Lin <slin55055@gmail.com>
2017-12-09 10:59:39 -0500

    did the list hover thing

--- intro.js
+++ intro.js
@@ -2,16 +2,23 @@ var b = document.getElementById("b");
 var thelist = document.getElementById("thelist");
 var h = document.getElementById("h");
 
-var changeHead = function() {
+var changeHead = function(e) {
+	console.log(this);
+	h.innerHTML = this.innerHTML;
 }
 
-var onClick = function() {
+var onClick = function(e) {
 	var item = document.createElement("li");
 	item.innerHTML = "New thing";
 	
-	item.addEventListener("onmouseenter", 
+	item.addEventListener("mouseenter", changeHead);
 	
 	thelist.appendChild(item);
 };
 
 b.addEventListener("click", onClick );
+
+//for...in is for object in general, for...of is for arrays
+for (let elem of thelist.children) {
+	elem.addEventListener("mouseenter", changeHead);
+}
