5a966dd9e8cd70951e5ae8b124067d0dffa6eccf
Mansour <melsharawy@stuy.edu>
2017-12-11 14:50:06 -0500

    wheels!

--- intro.js
+++ intro.js
@@ -1,5 +1,9 @@
 var b = document.getElementById("b");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
 var thelist = document.getElementById("thelist");
+var thenewlist = document.getElementById("thenewlist");
+var thenewerlist = document.getElementById("thenewerlist");
 var h = document.getElementById("h");
 
 var changeHead = function(e) {
@@ -14,18 +18,60 @@ var resetHead = function(e){
 
 var onClick = function(e) {
     var item = document.createElement("li");
-    item.innerHTML = "item " + thelist.children.length;
+    if(this.id == "b"){
+	if(e.deltaY > 0){
+	    item.innerHTML = "item " + thelist.children.length;
+	    item.addEventListener("mouseover", changeHead);
+	    item.addEventListener("click",removeol);
+	    thelist.appendChild(item);
+	}
+	else{
+	    thelist.lastElementChild.remove();
+	}
+    }
+    else if (this.id == "b2"){
+	if(e.deltaY > 0){
+	    item.innerHTML = fibonacci(thenewlist.children.length);
+	    thenewlist.appendChild(item);
+	}
+	else{
+	    thenewlist.lastElementChild.remove();
+	}
+    }
+    else{
+	console.log(e.deltaY);
+	if(e.deltaY > 0){
+	    item.innerHTML = triangle(thenewerlist.children.length);
+	    thenewerlist.appendChild(item);
+	}
+	else{
+	    thenewerlist.lastElementChild.remove();
+	}
+    }
 	
-    item.addEventListener("mouseover", changeHead);
-    item.addEventListener("click",removeol);
-	
-    thelist.appendChild(item);
+};
+
+var triangle = function(n){
+    return (n*(n+1))/2;
+};
+
+var fibonacci = function(n){
+
+    if(n < 2){
+	return 1;
+    }
+
+    else{
+	return fibonacci(n-1) + fibonacci(n-2);
+    }
 };
 
 var removeol = function(e){
     this.remove();
 }
-b.addEventListener("click", onClick );
+b.addEventListener("wheel", onClick );
+b2.addEventListener("wheel", onClick );
+b3.addEventListener("wheel", onClick );
 
 //for...in is for object in general, for...of is for arrays
 thelist.addEventListener("mouseout", resetHead);
